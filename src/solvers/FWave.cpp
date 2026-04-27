9c0a75d8af5bd493330caa0c4e7e28ae74fdc50c
Walipp <ph.waechter@web.de>
2026-04-27 20:51:27 +0200

    reflective boundary conditions

--- src/solvers/FWave.cpp
+++ src/solvers/FWave.cpp
@@ -76,6 +76,18 @@ void tsunami_lab::solvers::FWave::netUpdates( t_real i_hL,
                                             t_real i_bR,
                                             t_real o_netUpdateL[2],
                                             t_real o_netUpdateR[2] ) {
+  
+  if(i_bL >= 0) {
+    i_hL = i_hR;
+    i_huL = -i_huR;
+    i_bL = i_bR;
+  }
+  if(i_bR >= 0) {
+    i_hR = i_hL;
+    i_huR = -i_huL;
+    i_bR = i_bR;
+  }
+  
   // compute particle velocities
   t_real l_uL = i_huL / i_hL;
   t_real l_uR = i_huR / i_hR;
