a2c750d4789d21c6753b78b6a6b146b172bcb0f2
Anna Carina Otto <anna.carina.otto@uni-jena.de>
2026-05-06 21:11:26 +0200

    fix: Wave Propagation Merge fixed

--- src/patches/WavePropagation2d.cpp
+++ src/patches/WavePropagation2d.cpp
@@ -2,24 +2,40 @@
 <<<<<<< HEAD
  * @section DESCRIPTION
  * Two-dimensional wave propagation patch.
+ * Minimal 2D surrogate implementation: apply 1D updates along x for each
+ * row independently. This is not a full 2D method but sufficient to
+ * produce comparable time series along transects for educational tests.
  **/
 
- #include "WavePropagation2d.h"
- #include "../solvers/FWave.h"
+#include "WavePropagation2d.h"
+#include "../macros.h"
+#include "../solvers/FWave.h"
+#include "../solvers/Roe.h"
 
- tsunami_lab::patches::WavePropagation2d::WavePropagation2d(t_idx i_nCells){
-    m_nCells = i_nCells;
+#include <cstring>
+#include <algorithm>
+
+ tsunami_lab::patches::WavePropagation2d::WavePropagation2d(t_idx i_nx, t_idx i_ny){
+    m_nx = i_nx;
+    m_ny = i_ny;
 
     // allocate memory including a single ghost cell on each side
+    t_idx l_stride = m_nx + 2; // include ghost cells in x
+
     for( unsigned short l_st = 0; l_st < 2; l_st++ ) {
-        m_h[l_st] = new t_real[  m_nCells + 2 ];
-        m_hu[l_st] = new t_real[ m_nCells + 2 ];
-        m_hv[l_st] = new t_real[ m_nCells + 2 ];
+        m_h[l_st] = new t_real[ (m_ny) * l_stride];
+        m_hu[l_st] = new t_real[ (m_ny) * l_stride ];
+        m_hv[l_st] = new t_real[ (m_ny) * l_stride ];
     }
-    m_b = new t_real[  m_nCells + 2 ];
+    m_b = new t_real[  (m_ny) * l_stride ];
+
+
+    m_compactH = new t_real[ m_ny * m_nx ];
+    m_compactHu = new t_real[ m_ny * m_nx ];
+    m_compactHv = new t_real[ m_ny * m_nx ];
 
     // init to zero
-    for( t_idx l_ce = 0; l_ce < m_nCells; l_ce++ ) {
+    for( t_idx l_ce = 0; l_ce < ((m_ny) * l_stride); l_ce++ ) {
         for( unsigned short l_st = 0; l_st < 2; l_st++ ) {
           m_h[l_st][l_ce] = 0;
           m_hu[l_st][l_ce] = 0;
@@ -36,28 +52,37 @@ tsunami_lab::patches::WavePropagation2d::~WavePropagation2d() {
         delete [] m_hv[l_st];
     }
     delete[] m_b;
+    delete[] m_compactH;
+    delete[] m_compactHu;
+    delete[] m_compactHv;
 }
 
 void tsunami_lab::patches::WavePropagation2d::timeStep(t_real i_scaling){
+    t_idx l_stride = m_nx + 2; // include ghost cells in x
+
     // pointers to old and new data
     t_real * l_hOld = m_h[m_step];
     t_real * l_huOld = m_hu[m_step];
     t_real * l_hvOld = m_hv[m_step];
 
     m_step = (m_step+1) % 2;
+
     t_real * l_hNew =  m_h[m_step];
     t_real * l_huNew = m_hu[m_step];
     t_real * l_hvNew = m_hv[m_step];
 
     // init new cell quantities
-    for( t_idx l_ce = 1; l_ce < m_nCells+1; l_ce++ ) {
+    for( t_idx l_ce = 1; l_ce < m_ny * l_stride; l_ce++ ) {
         l_hNew[l_ce] = l_hOld[l_ce];
         l_huNew[l_ce] = l_huOld[l_ce];
         l_hvNew[l_ce] = l_hvOld[l_ce];
     }
 
     // iterate over edges and update with Riemann solutions
-    for( t_idx l_ed = 0; l_ed < m_nCells+1; l_ed++ ) {
+
+    for( t_idx l_row = 0; l_row < m_ny; l_row++ ){
+      t_idx l_off = l_row * l_stride;
+      for( t_idx l_ed = 0; l_ed < m_nx; l_ed++ ) {
         // determine left and right cell-id
         t_idx l_ceL = l_ed;
         t_idx l_ceR = l_ed+1;
@@ -65,29 +90,29 @@ void tsunami_lab::patches::WavePropagation2d::timeStep(t_real i_scaling){
         // compute net-updates f-wave
         //provides hu and hu²+1/2gh²
         t_real l_netUpdatesA[2][2];
-        solvers::FWave::netUpdates( l_hOld[l_ceL],
-                              l_hOld[l_ceR],
-                              l_huOld[l_ceL],
-                              l_huOld[l_ceR],
-                              m_b[l_ceL],
-                              m_b[l_ceR],
+        solvers::FWave::netUpdates( l_hOld[l_off + l_ceL],
+                              l_hOld[l_off + l_ceR],
+                              l_huOld[l_off + l_ceL],
+                              l_huOld[l_off + l_ceR],
+                              m_b[l_off + l_ceL],
+                              m_b[l_off + l_ceR],
                               l_netUpdatesA[0],
                               l_netUpdatesA[1] );
 
         //provides hv and hv²+1/2gh²
         t_real l_netUpdatesB[2][2];
-        solvers::FWave::netUpdates( l_hOld[l_ceL],
-                              l_hOld[l_ceR],
-                              l_hvOld[l_ceL],
-                              l_hvOld[l_ceR],
-                              m_b[l_ceL],
-                              m_b[l_ceR],
+        solvers::FWave::netUpdates( l_hOld[l_off + l_ceL],
+                              l_hOld[l_off + l_ceR],
+                              l_hvOld[l_off + l_ceL],
+                              l_hvOld[l_off + l_ceR],
+                              m_b[l_off + l_ceL],
+                              m_b[l_off + l_ceR],
                               l_netUpdatesB[0],
                               l_netUpdatesB[1] );
 
         // compute huv
-        t_real l_huvL = (l_huOld[l_ceL] * l_hvOld[l_ceL])/l_hOld[l_ceL];
-        t_real l_huvR = (l_huOld[l_ceR] * l_hvOld[l_ceR])/l_hOld[l_ceR];
+        t_real l_huvL = (l_huOld[l_off + l_ceL] * l_hvOld[l_off + l_ceL])/l_hOld[l_off + l_ceL];
+        t_real l_huvR = (l_huOld[l_off + l_ceR] * l_hvOld[l_off + l_ceR])/l_hOld[l_off + l_ceR];
 
         // update the cells' quantities
         /* | h  |   |     hu        |   |      hv       |
@@ -95,164 +120,14 @@ void tsunami_lab::patches::WavePropagation2d::timeStep(t_real i_scaling){
         *  | hv |   |     huv       |   | hv² + 1/2gh²  |
         */
 
-        l_hNew[l_ceL]  -= i_scaling * l_netUpdatesA[0][0] - i_scaling * l_netUpdatesB[0][0];
-        l_huNew[l_ceL] -= i_scaling * l_netUpdatesA[0][1] - i_scaling * l_huvL;
-        l_hvNew[l_ceL] -= i_scaling * l_huvL - i_scaling * l_netUpdatesB[0][1];
+        l_hNew[l_off + l_ceL]  -= i_scaling * l_netUpdatesA[0][0] - i_scaling * l_netUpdatesB[0][0];
+        l_huNew[l_off + l_ceL] -= i_scaling * l_netUpdatesA[0][1] - i_scaling * l_huvL;
+        l_hvNew[l_off + l_ceL] -= i_scaling * l_huvL - i_scaling * l_netUpdatesB[0][1];
 
-        l_hNew[l_ceR]  -= i_scaling * l_netUpdatesA[1][0] - i_scaling * l_netUpdatesB[1][0];
-        l_huNew[l_ceR] -= i_scaling * l_netUpdatesA[1][1] - i_scaling * l_huvR;
-        l_hvNew[l_ceR] -= i_scaling * l_huvR - i_scaling * l_netUpdatesB[1][1];
+        l_hNew[l_off + l_ceR]  -= i_scaling * l_netUpdatesA[1][0] - i_scaling * l_netUpdatesB[1][0];
+        l_huNew[l_off + l_ceR] -= i_scaling * l_netUpdatesA[1][1] - i_scaling * l_huvR;
+        l_hvNew[l_off + l_ceR] -= i_scaling * l_huvR - i_scaling * l_netUpdatesB[1][1];
 
-    }
-}
-
-void tsunami_lab::patches::WavePropagation2d::setGhostOutflow() {
-=======
- * Minimal 2D surrogate implementation: apply 1D updates along x for each
- * row independently. This is not a full 2D method but sufficient to
- * produce comparable time series along transects for educational tests.
- **/
-
-#include "WavePropagation2d.h"
-#include "../macros.h"
-#include "../solvers/FWave.h"
-#include "../solvers/Roe.h"
-
-#include <cstring>
-#include <algorithm>
-
-tsunami_lab::patches::WavePropagation2d::WavePropagation2d( t_idx i_nx, t_idx i_ny ) {
-  m_nx = i_nx;
-  m_ny = i_ny;
-
-  t_idx l_stride = m_nx + 2; // include ghost cells in x
-
-  for( unsigned short l = 0; l < 2; l++ ){
-    m_h[l] = new t_real[ (m_ny) * l_stride ];
-    m_hu[l] = new t_real[ (m_ny) * l_stride ];
-    m_hv[l] = new t_real[ (m_ny) * l_stride ];
-  }
-  m_b = new t_real[ (m_ny) * l_stride ];
-
-  m_compactH = new t_real[ m_ny * m_nx ];
-  m_compactHu = new t_real[ m_ny * m_nx ];
-  m_compactHv = new t_real[ m_ny * m_nx ];
-
-  // initialize
-  std::memset( m_h[0], 0, sizeof(t_real) * m_ny * l_stride );
-  std::memset( m_h[1], 0, sizeof(t_real) * m_ny * l_stride );
-  std::memset( m_hu[0], 0, sizeof(t_real) * m_ny * l_stride );
-  std::memset( m_hu[1], 0, sizeof(t_real) * m_ny * l_stride );
-  std::memset( m_hv[0], 0, sizeof(t_real) * m_ny * l_stride );
-  std::memset( m_hv[1], 0, sizeof(t_real) * m_ny * l_stride );
-  std::memset( m_b, 0, sizeof(t_real) * m_ny * l_stride );
-  std::memset( m_compactH, 0, sizeof(t_real) * m_ny * m_nx );
-  std::memset( m_compactHu, 0, sizeof(t_real) * m_ny * m_nx );
-  std::memset( m_compactHv, 0, sizeof(t_real) * m_ny * m_nx );
-}
-
-tsunami_lab::patches::WavePropagation2d::~WavePropagation2d(){
-  for( unsigned short l = 0; l < 2; l++ ){
-    delete[] m_h[l];
-    delete[] m_hu[l];
-    delete[] m_hv[l];
-  }
-  delete[] m_b;
-  delete[] m_compactH;
-  delete[] m_compactHu;
-  delete[] m_compactHv;
-}
-
-void tsunami_lab::patches::WavePropagation2d::setGhostOutflow(){
-  t_idx l_stride = m_nx + 2;
->>>>>>> origin/feat/added-documentation
-  t_real * l_h = m_h[m_step];
-  t_real * l_hu = m_hu[m_step];
-  t_real * l_hv = m_hv[m_step];
-
-<<<<<<< HEAD
-  // set left boundary
-  l_h[0] = l_h[1];
-  l_hu[0] = l_hu[1];
-  l_hv[0] = l_hv[1];
-
-  // set right boundary
-  l_h[m_nCells+1] = l_h[m_nCells];
-  l_hu[m_nCells+1] = l_hu[m_nCells];
-  l_hv[m_nCells+1] = l_hv[m_nCells];
-}
-=======
-  for( t_idx l_row = 0; l_row < m_ny; l_row++ ){
-    t_idx l_off = l_row * l_stride;
-    // left ghost
-    l_h[l_off + 0] = l_h[l_off + 1];
-    l_hu[l_off + 0] = l_hu[l_off + 1];
-    l_hv[l_off + 0] = l_hv[l_off + 1];
-    // right ghost
-    l_h[l_off + m_nx + 1] = l_h[l_off + m_nx];
-    l_hu[l_off + m_nx + 1] = l_hu[l_off + m_nx];
-    l_hv[l_off + m_nx + 1] = l_hv[l_off + m_nx];
-  }
-}
-
-void tsunami_lab::patches::WavePropagation2d::timeStep( t_real i_scaling ){
-  t_idx l_stride = m_nx + 2;
-  t_real * l_hOld = m_h[m_step];
-  t_real * l_huOld = m_hu[m_step];
-  t_real * l_hvOld = m_hv[m_step];
-
-  m_step = (m_step+1) % 2;
-
-  t_real * l_hNew = m_h[m_step];
-  t_real * l_huNew = m_hu[m_step];
-  t_real * l_hvNew = m_hv[m_step];
-
-  // copy old to new
-  std::memcpy( l_hNew, l_hOld, sizeof(t_real) * m_ny * l_stride );
-  std::memcpy( l_huNew, l_huOld, sizeof(t_real) * m_ny * l_stride );
-  std::memcpy( l_hvNew, l_hvOld, sizeof(t_real) * m_ny * l_stride );
-
-  // perform 1D updates on each row independently
-  for( t_idx l_row = 0; l_row < m_ny; l_row++ ){
-    t_idx l_off = l_row * l_stride;
-    for( t_idx l_ed = 0; l_ed < m_nx+1; l_ed++ ){
-      t_idx l_ceL = l_ed;
-      t_idx l_ceR = l_ed+1;
-
-      t_real l_hL = l_hOld[l_off + l_ceL];
-      t_real l_hR = l_hOld[l_off + l_ceR];
-      t_real l_huL = l_huOld[l_off + l_ceL];
-      t_real l_huR = l_huOld[l_off + l_ceR];
-
-      t_real l_netUpdates[2][2];
-
-      switch(MODE){
-        case MODEFWAVE:
-          solvers::FWave::netUpdates( l_hL,
-                                      l_hR,
-                                      l_huL,
-                                      l_huR,
-                                      m_b[l_off + l_ceL],
-                                      m_b[l_off + l_ceR],
-                                      l_netUpdates[0],
-                                      l_netUpdates[1] );
-          break;
-        case MODEROE:
-          solvers::Roe::netUpdates( l_hL,
-                                    l_hR,
-                                    l_huL,
-                                    l_huR,
-                                    l_netUpdates[0],
-                                    l_netUpdates[1] );
-          break;
-      }
-
-      // update
-      l_hNew[l_off + l_ceL]  -= i_scaling * l_netUpdates[0][0];
-      l_huNew[l_off + l_ceL] -= i_scaling * l_netUpdates[0][1];
-
-      l_hNew[l_off + l_ceR]  -= i_scaling * l_netUpdates[1][0];
-      l_huNew[l_off + l_ceR] -= i_scaling * l_netUpdates[1][1];
     }
   }
 
@@ -267,21 +142,42 @@ void tsunami_lab::patches::WavePropagation2d::timeStep( t_real i_scaling ){
       m_compactHv[l_dst] = l_hvNew[l_src];
     }
   }
+
 }
 
-t_idx tsunami_lab::patches::WavePropagation2d::getStride(){
+void tsunami_lab::patches::WavePropagation2d::setGhostOutflow() {
+
+  t_idx l_stride = m_nx + 2;
+  t_real * l_h = m_h[m_step];
+  t_real * l_hu = m_hu[m_step];
+  t_real * l_hv = m_hv[m_step];
+  
+  for( t_idx l_row = 0; l_row < m_ny; l_row++ ){
+    t_idx l_off = l_row * l_stride;
+    // left ghost
+    l_h[l_off + 0] = l_h[l_off + 1];
+    l_hu[l_off + 0] = l_hu[l_off + 1];
+    l_hv[l_off + 0] = l_hv[l_off + 1];
+    // right ghost
+    l_h[l_off + m_nx + 1] = l_h[l_off + m_nx];
+    l_hu[l_off + m_nx + 1] = l_hu[l_off + m_nx];
+    l_hv[l_off + m_nx + 1] = l_hv[l_off + m_nx];
+  }
+}
+
+tsunami_lab::t_idx tsunami_lab::patches::WavePropagation2d::getStride(){
   return m_nx;
 }
 
-t_real const * tsunami_lab::patches::WavePropagation2d::getHeight(){
+tsunami_lab::t_real const * tsunami_lab::patches::WavePropagation2d::getHeight(){
   return m_compactH;
 }
 
-t_real const * tsunami_lab::patches::WavePropagation2d::getMomentumX(){
+tsunami_lab::t_real const * tsunami_lab::patches::WavePropagation2d::getMomentumX(){
   return m_compactHu;
 }
 
-t_real const * tsunami_lab::patches::WavePropagation2d::getMomentumY(){
+tsunami_lab::t_real const * tsunami_lab::patches::WavePropagation2d::getMomentumY(){
   return m_compactHv;
 }
 
@@ -299,4 +195,3 @@ void tsunami_lab::patches::WavePropagation2d::setMomentumY( t_idx i_ix, t_idx i_
   t_idx l_stride = m_nx + 2;
   m_hv[m_step][ i_iy * l_stride + 1 + i_ix ] = i_hv;
 }
->>>>>>> origin/feat/added-documentation
--- src/patches/WavePropagation2d.h
+++ src/patches/WavePropagation2d.h
@@ -1,21 +1,15 @@
 /**
-<<<<<<< HEAD
  * @section DESCRIPTION
  * Two-dimensional wave propagation patch.
-=======
  * Simple 2D wave propagation patch implemented as independent 1D
  * sweeps in x-direction for each row. This is intended as a minimal
  * 2D surrogate for comparison along a central transect with the
  * existing 1D solver.
->>>>>>> origin/feat/added-documentation
  **/
 #ifndef TSUNAMI_LAB_PATCHES_WAVE_PROPAGATION_2D
 #define TSUNAMI_LAB_PATCHES_WAVE_PROPAGATION_2D
 
-<<<<<<< HEAD
 #include "../constants.h"
-=======
->>>>>>> origin/feat/added-documentation
 #include "WavePropagation.h"
 
 namespace tsunami_lab {
@@ -24,14 +18,14 @@ namespace tsunami_lab {
   }
 }
 
-<<<<<<< HEAD
 class tsunami_lab::patches::WavePropagation2d: public WavePropagation{
     private:
         //! current step which indicates the active values in the arrays below
         unsigned short m_step = 0;
 
         //! number of cells discretizing the computational domain
-        t_idx m_nCells = 0;
+        t_idx m_nx = 0;
+        t_idx m_ny = 0;
 
         //! water heights for the current and next time step for all cells
         t_real * m_h[2] = { nullptr, nullptr };
@@ -43,15 +37,21 @@ class tsunami_lab::patches::WavePropagation2d: public WavePropagation{
         t_real * m_hv[2] = { nullptr, nullptr };
 
         //! bathymetric data for all cells
-        t_real * m_b;        
+        t_real * m_b; 
+        
+        // compact buffers without ghost cells (for CSV output)
+        t_real * m_compactH = nullptr;
+        t_real * m_compactHu = nullptr;
+        t_real * m_compactHv = nullptr;
     
     public:
         /**
         * CONSTRUCTOR
         *
-        * @param i_nCells number of cells
+        * @param i_nx number of cells in x-direction
+        * @param i_nx number of cells in y-direction
         */
-        WavePropagation2d( t_idx i_nCells);
+        WavePropagation2d( t_idx i_nx, t_idx i_ny);
 
         /**
         * DECONSTRUCTOR
@@ -76,132 +76,73 @@ class tsunami_lab::patches::WavePropagation2d: public WavePropagation{
          *
          * @return stride in y-direction.
          **/
-        t_idx getStride(){
-          return m_nCells+2;
-        }
+        t_idx getStride();
 
         /**
          * Gets cells' water heights.
          *
          * @return water heights.
          */
-        t_real const * getHeight(){
-          return m_h[m_step]+1;
-        }
+        t_real const * getHeight();
 
         /**
          * Gets the cells' momenta in x-direction.
          *
          * @return momenta in x-direction.
          **/
-        t_real const * getMomentumX(){
-          return m_hu[m_step]+1;
-        }
+        t_real const * getMomentumX();
 
         /**
          * Gets the cells' momenta in y-direction.
          *
          * @return momenta in y-direction.
          **/
-        t_real const * getMomentumY(){
-          return m_hv[m_step]+1;
-        }
+        t_real const * getMomentumY();
 
         /**
          * Sets the height of the cell to the given value.
          *
          * @param i_ix id of the cell in x-direction.
+         * @param i_iy id of the cell in y-direction.
          * @param i_h water height.
          **/
         void setHeight( t_idx  i_ix,
-                        t_idx,
-                        t_real i_h ) {
-          m_h[m_step][i_ix+1] = i_h;
-        }
+                    t_idx  i_iy,
+                    t_real i_h );
 
         /**
          * Sets the momentum in x-direction to the given value.
          *
          * @param i_ix id of the cell in x-direction.
+         * @param i_iy id of the cell in y-direction.
          * @param i_hu momentum in x-direction.
          **/
         void setMomentumX( t_idx  i_ix,
-                           t_idx,
-                           t_real i_hu ) {
-          m_hu[m_step][i_ix+1] = i_hu;
-        }
+                       t_idx  i_iy,
+                       t_real i_hu );
 
         /**
          * Sets the momentum in y-direction to the given value.
          *
+         * @param i_ix id of the cell in x-direction.
          * @param i_iy id of the cell in y-direction.
          * @param i_hv momentum in y-direction.
          **/
-        void setMomentumY( t_idx  i_iy,
-                           t_idx,
-                           t_real i_hv ) {
-          m_hu[m_step][i_iy+1] = i_hv;
-        }
+        void setMomentumY( t_idx  i_ix,
+                       t_idx  i_iy,
+                       t_real i_hv );
 
         /**
          * Sets the bathymetry to the given value.
          * @param i_ix id of the cell in x-direction.
+         * @param i_iy id of the cell in y-direction.
          * @param i_b  bathymetry.
          **/
         void setBathymetry( t_idx i_ix,
-                            t_idx,
+                            t_idx i_iy,
                             t_real i_b) {
           m_b[i_ix] = i_b;
         }
 };
 
 #endif
-=======
-class tsunami_lab::patches::WavePropagation2d: public WavePropagation {
-  private:
-    unsigned short m_step = 0;
-    t_idx m_nx = 0;
-    t_idx m_ny = 0;
-
-    // storage includes two time layers; data layout per layer:
-    // row-major with a single ghost cell at left and right for each row
-    // internal stride = m_nx + 2
-    t_real * m_h[2] = { nullptr, nullptr };
-    t_real * m_hu[2] = { nullptr, nullptr };
-    t_real * m_hv[2] = { nullptr, nullptr };
-    t_real * m_b = nullptr;
-
-    // compact buffers without ghost cells (for CSV output)
-    t_real * m_compactH = nullptr;
-    t_real * m_compactHu = nullptr;
-    t_real * m_compactHv = nullptr;
-
-  public:
-    WavePropagation2d( t_idx i_nx, t_idx i_ny );
-    ~WavePropagation2d();
-
-    void timeStep( t_real i_scaling );
-    void setGhostOutflow();
-
-    // returns stride for compact arrays (number of columns)
-    t_idx getStride();
-
-    t_real const * getHeight();
-    t_real const * getMomentumX();
-    t_real const * getMomentumY();
-
-    void setHeight( t_idx  i_ix,
-                    t_idx  i_iy,
-                    t_real i_h );
-
-    void setMomentumX( t_idx  i_ix,
-                       t_idx  i_iy,
-                       t_real i_hu );
-
-    void setMomentumY( t_idx  i_ix,
-                       t_idx  i_iy,
-                       t_real i_hv );
-};
-
-#endif
->>>>>>> origin/feat/added-documentation
